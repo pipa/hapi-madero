9bbb8bf33c97b823c44a335620b9261fa490c4de
Luis Matute <luis.matute@me.com>
2017-09-20 08:35:32 -0600

    - [Update] README to show how to log all responses - [Update] All request logs now logs actual URL (previously just logging url path) - [Update] Added isServer and isDeveloperError to error logs - [Update] Now correctly gets session ID - [Update] minor corrections to tags

--- README.md
+++ README.md
@@ -51,6 +51,29 @@ server.register({ register: Madero, options: maderoOptions }, err => {
         }
     });
 
+    // Logging all responses
+    server.on('response', (request) => {
+
+        const response = request.response;
+        let statusCode = response.statusCode;
+        let message = 'Auto request log';
+        let entry = { statusCode, message };
+        let error;
+
+        if (response.isBoom) {
+            statusCode = response.output.payload.statusCode;
+            message = response.message;
+            error = response.output.payload;
+            entry = { statusCode, message, error };
+
+            // Logs error
+            return request.log(['error', 'boom'], entry);
+        }
+
+        // Log every request
+        return request.log(['info'], entry);
+    });
+
     server.start(err => {
 
         if (err) {
--- test.js
+++ test.js
@@ -27,7 +27,7 @@ server.register({ register: Madero, options: internals.settings }, err => {
         path: '/log-test',
         handler: (request, reply) => {
 
-            request.log(['test'], { message: 'test', foo: 'bar' });
+            // request.log(['test'], { message: 'test', foo: 'bar' });
             // request.log(['test'], 'this is a test');
 
             return reply('ok');
@@ -41,6 +41,29 @@ server.register({ register: Madero, options: internals.settings }, err => {
 
             return reply(Boom.badImplementation('Test error', new Error('test')));
         }
+    });
+
+    // Logging all responses
+    server.on('response', (request) => {
+
+        const response = request.response;
+        let statusCode = response.statusCode;
+        let message = 'Auto request log';
+        let entry = { statusCode, message };
+        let error;
+
+        if (response.isBoom) {
+            statusCode = response.output.payload.statusCode;
+            message = response.message;
+            error = response.output.payload;
+            entry = { statusCode, message, error };
+
+            // Logs error
+            return request.log(['error', 'boom'], entry);
+        }
+
+        // Log every request
+        return request.log(['info'], entry);
     });
 
     server.start(err => {
